186e9ebc02225f5c2934dfbf46577fd946d0b8ec
bkellerman <bert@ciphertrace.com>
2020-10-27 16:54:12 -0700

    leaks.sh

--- .ci/leaks.sh
+++ .ci/leaks.sh
@@ -2,7 +2,7 @@
 
 if [ ! -z $TRAVIS_PULL_REQUEST ]; then
     REPO_SLUG="/${TRAVIS_REPO_SLUG}"
-
+    echo "Building for slug ${TRAVIS_REPO_SLUG}, commit ${TRAVIS_COMMIT}"
     # Audit the current commit for secrets
     docker run --rm --name=gitleaks -v $PWD:$REPO_SLUG zricethezav/gitleaks -v --repo-path=$REPO_SLUG --commit=$TRAVIS_COMMIT
 fi
