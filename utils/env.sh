fab2af013b05e08c66a6b91f4f17cfa5c28dde42
bk <bert@reflexer.finance>
2021-02-11 10:13:39 -0800

    Update env.sh

--- utils/env.sh
+++ utils/env.sh
@@ -1,4 +1,4 @@
-#export ETH_RPC_URL="https://kovan.infura.io/v3/411777692d554c2f82bd519c4e660491"
-export ETH_RPC_URL="http://parity-kovan.reflexer.finance:8545"
-export ETH_ACCOUNT='0xdD1693BD8E307eCfDbe51D246562fc4109f871f8'
-export ETH_KEYPASS='key_file=/Users/georgekellerman/reflexer/keystore/key.json,pass_file=/Users/georgekellerman/reflexer/keystore/p.txt'
+
+export ETH_RPC_URL=''
+export ETH_ACCOUNT=''
+export ETH_KEYPASS=''
