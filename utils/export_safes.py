768936d9d47cfc0e63e217e4b3b6295c82195d03
bkellerman <bertkellerman@gmail.com>
2022-06-22 13:57:20 -0400

    add safe_id to export_safes

--- utils/export_safes.py
+++ utils/export_safes.py
@@ -17,18 +17,18 @@ safes = []
 #SAFE('0x0000000000000000000000000000000000000000')[[] locked_collateral=0.000000000000000000 generated_debt=0.000000000000000000]
 safe_id = 1
 while True:
-    print(f"procesing {safe_id=}")
+    print(f"procesing {safe_id}")
     safe = geb.safe_manager.safe(safe_id)
     owner = geb.safe_manager.owns_safe(safe_id)
 
     if owner.address == '0x0000000000000000000000000000000000000000':
         break
 
-    safes.append((safe.address.address, owner.address, str(safe.locked_collateral), str(safe.generated_debt)))
+    safes.append((safe_id, safe.address.address, owner.address, str(safe.locked_collateral), str(safe.generated_debt)))
 
     safe_id += 1
 
 with open(out_file, 'w') as f:
     write = csv.writer(f)
-    write.writerow(['safe', 'owner', 'collateral', 'debt'])
+    write.writerow(['safe_id', 'safe', 'owner', 'collateral', 'debt'])
     write.writerows(safes)
